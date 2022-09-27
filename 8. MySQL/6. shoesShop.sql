7693e466647bd48e5d0cd2b864f284b22ffbec93
NickPesht <114055954+NickPesht@users.noreply.github.com>
2022-09-27 15:00:04 +0700

    Update 6. shoesShop.sql

--- 8. MySQL/6. shoesShop.sql
+++ 8. MySQL/6. shoesShop.sql
@@ -1,8 +1,8 @@
 /* 
-	Вопрос-1 домашнего задания-4:
-	Создать базу данных магазина из 2 таблиц - продукты (товары) и категории товаров.
-    Каждый товар принадлежит одной категории.
-    Вставьте данные в таблицы (3 - 5 строк). 
+Вопрос-1 домашнего задания-4:
+Создать базу данных магазина из 2 таблиц - продукты (товары) и категории товаров.
+Каждый товар принадлежит одной категории.
+Вставьте данные в таблицы (3 - 5 строк). 
 */
 
 CREATE DATABASE shoesShop;
@@ -11,26 +11,26 @@ USE shoesShop;
 
 CREATE TABLE productCategory
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE product
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     model VARCHAR(25) NOT NULL UNIQUE,
     price MEDIUMINT NOT NULL,
     productCategoryId INT NOT NULL,
     FOREIGN KEY (productCategoryId) REFERENCES productCategory(id)
-		ON UPDATE NO ACTION
+	ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
 INSERT INTO productCategory (name)
 VALUES 
-	('Ботинки мужские'), 
-	('Кеды All Star'), 
+    ('Ботинки мужские'), 
+    ('Кеды All Star'), 
     ('Кеды All Star Chuck Taylor'),
     ('Кроссовки мужские'), 
     ('Полуботинки мужские'), 
@@ -38,17 +38,17 @@ VALUES
 
 INSERT INTO product (name, model, price, productCategoryId)
 VALUES
-	('Ботинки мужские FRAZIER', 'FR22-23', 98, 1),
-	('Кеды Convers All Star летние низкие', 'M5039', 36, 2),
+    ('Ботинки мужские FRAZIER', 'FR22-23', 98, 1),
+    ('Кеды Convers All Star летние низкие', 'M5039', 36, 2),
     ('Кеды Converse All Star Chuck Taylor летние', 'M9160', 39, 3),
     ('Кроссовки мужские GREY', 'GR22-23', 85, 4),
     ('Кроссовки ECCO ZIPFLEX M', 'M803824', 129, 5),
     ('Полуботинки ECCO ST.1 HYBRID', 'M836794', 95, 6);
 
 /*
-	Попробовать UPDATE, DELETE:
-    Удалить товары с ценой выше 100.
-    Изменить название и цену некоторому товару.
+Попробовать UPDATE, DELETE:
+Удалить товары с ценой выше 100.
+Изменить название и цену некоторому товару.
 */
 
 DELETE FROM product
@@ -56,6 +56,6 @@ WHERE price > 100;
 
 UPDATE product
 SET 
-	price = price * 2.25, 
-	name = 'Кеды Convers Dainty зимние' 
+    price = price * 2.25, 
+    name = 'Кеды Convers Dainty зимние' 
 WHERE id = 3;
