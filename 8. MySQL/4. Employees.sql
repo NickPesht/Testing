d2e0849dba6c24337713c221aa05c6ba851d837c
NickPesht <114055954+NickPesht@users.noreply.github.com>
2022-09-27 14:55:57 +0700

    Update 4. Employees.sql

--- 8. MySQL/4. Employees.sql
+++ 8. MySQL/4. Employees.sql
@@ -19,16 +19,16 @@ salary - число
 */
 
 SELECT emp1.name AS employee_name, 
-	d1.name AS employee_department,
-	emp2.name AS chief_name,
+    d1.name AS employee_department,
+    emp2.name AS chief_name,
     d2.name AS chief_department
 FROM employees AS emp1
 LEFT JOIN departments AS d1
-	ON d1.id = emp1.departments_id
+    ON d1.id = emp1.departments_id
 LEFT JOIN employees AS emp2
-	ON emp1.chief_id = emp2.id
+    ON emp1.chief_id = emp2.id
 LEFT JOIN departments AS d2
-	ON d2.id = emp2.departments_id
+    ON d2.id = emp2.departments_id
 ORDER BY emp1.name;
 
  /*
@@ -40,7 +40,7 @@ ORDER BY emp1.name;
 SELECT emp1.name 
 FROM employees AS emp1
 LEFT JOIN employees AS chief
-	ON chief.id = emp1.chief_id
+    ON chief.id = emp1.chief_id
 WHERE emp1.salary > chief.salary;
 
 /* 
@@ -54,15 +54,16 @@ WHERE emp1.salary > chief.salary;
 SELECT name
 FROM employees
 WHERE (departments_id, salary) IN (SELECT departments_id, MAX(salary)
-															FROM employees
-															GROUP BY departments_id);
+				   FROM employees
+				   GROUP BY departments_id);
+				   
 -- Вариант решения-2:
 
 SELECT emp1.name
 FROM  employees AS emp1
 WHERE emp1.salary = (SELECT MAX(salary) 
-										FROM employees AS emp2
-										WHERE  emp2.departments_id = emp1.departments_id);
+		     FROM employees AS emp2
+		     WHERE emp2.departments_id = emp1.departments_id);
 
 /*
 Задача-4.
@@ -73,7 +74,7 @@ WHERE emp1.salary = (SELECT MAX(salary)
 SELECT d.name
 FROM departments AS d
 LEFT JOIN employees AS e
-	ON e.departments_id = d.id
+    ON e.departments_id = d.id
 GROUP BY d.name
 HAVING COUNT(e.id) <= 3;
 
@@ -86,5 +87,5 @@ HAVING COUNT(e.id) <= 3;
 SELECT emp1.name
 FROM  employees AS emp1
 LEFT JOIN employees AS emp2 
-	ON emp2.id = emp1.chief_id AND emp2.departments_id = emp1.departments_id
+    ON emp2.id = emp1.chief_id AND emp2.departments_id = emp1.departments_id
 WHERE emp2.id IS NULL;
