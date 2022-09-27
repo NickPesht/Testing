f9f501d6de55a453eb1f6957750c4ed33c252ecf
NickPesht <114055954+NickPesht@users.noreply.github.com>
2022-09-27 14:45:07 +0700

    Update 1. testDataPesht.sql

--- 8. MySQL/1. testDataPesht.sql
+++ 8. MySQL/1. testDataPesht.sql
@@ -1,9 +1,9 @@
 /*
-	Задача "Тестовые данные".
-    Напишите скрипт, который заполнит БД из задачи Shop категориями и товарами.
-    Пусть создастся 100 категорий и 5000 товаров. 
-	Пусть товары привязываются к категориям случайным образом. 
-    И случайным образом пусть выставляется цена.
+Задача "Тестовые данные".
+Напишите скрипт, который заполнит БД из задачи Shop категориями и товарами.
+Пусть создастся 100 категорий и 5000 товаров.
+Пусть товары привязываются к категориям случайным образом. 
+И случайным образом пусть выставляется цена.
 */
 
 CREATE DATABASE Shop;
@@ -18,8 +18,8 @@ CREATE TABLE productCategory
 
 CREATE TABLE product
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     price MEDIUMINT NOT NULL,
     productCategoryId INT NOT NULL,
     FOREIGN KEY (productCategoryId) REFERENCES productCategory(id)
@@ -28,27 +28,27 @@ CREATE TABLE product
 DELIMITER $$
 CREATE PROCEDURE insertCategoriesData()
 BEGIN
-	DECLARE i INT DEFAULT 1;
+    DECLARE i INT DEFAULT 1;
     
     WHILE i <= 100 DO
-		INSERT INTO productCategory (name)
+	INSERT INTO productCategory (name)
         VALUES (CONCAT('Категория ', i));
         
         SET i = i + 1;
-	END WHILE;
+    END WHILE;
 END$$
 
 CREATE PROCEDURE insertProductsData()
 BEGIN
-	DECLARE i INT DEFAULT 1;
+    DECLARE i INT DEFAULT 1;
     
     WHILE i <= 5000 DO
-		INSERT INTO product (name, price, productCategoryId)
+	INSERT INTO product (name, price, productCategoryId)
         VALUES (
             CONCAT('Товар ', i),
             FLOOR(RAND() * 1000) + 1,
             FLOOR(RAND() * 100) + 1
-		);
+	);
         
         SET i = i + 1;
 	END WHILE;
